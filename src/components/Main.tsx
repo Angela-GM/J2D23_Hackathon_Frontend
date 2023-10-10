09b98b95f776ec60928357532a81ef66dfbd4808
Angela-GM <angelagm0112@gmail.com>
2023-10-10 17:05:02 +0200

    añadir comprobacion para que no sume 1 a page si es 42

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,50 +1,39 @@
-import { useEffect, useState } from "react"
-import { getAllCharacters } from "../services/characters"
+import { useEffect, useState } from "react";
+import { getAllCharacters } from "../services/characters";
 
 function Main() {
-    // Crear interface de allCharacters
-    const [allCharacters, setAllCharacters] = useState([])
-    const [page, setPage] = useState(2);
+  // Crear interface de allCharacters
+  const [allCharacters, setAllCharacters] = useState([]);
+  const [page, setPage] = useState(2);
 
-   useEffect(() => {
-     // llamada al servicio de api apiClient y guardar la informacion en allCharacters
-     const fetchCharacters = async () => {
-        try {
-            const response = await getAllCharacters(page);
-            const data = response.data;
-            setAllCharacters(data.results);
-            
-        } catch (error) {
-            console.error('Error al obtener los personajes:', error);
-            
-        }
-    }
+  useEffect(() => {
+    // llamada al servicio de api apiClient y guardar la informacion en allCharacters
+    const fetchCharacters = async () => {
+      try {
+        const response = await getAllCharacters(page);
+        const data = response.data;
+        setAllCharacters(data.results);
+      } catch (error) {
+        console.error("Error al obtener los personajes:", error);
+      }
+    };
 
     fetchCharacters();
-   }, [page])
-   console.log(allCharacters);
+  }, [page]);
+  console.log(allCharacters);
 
-   const hadleChangePage = () => {
-    return setPage(page + 1)
-
-   }
-   
- 
-
-              
-
-    
-
-
+  const hadleChangePage = () => {
+    if (page <= 41) {
+        setPage(page + 1);
+      }
+  };
 
   return (
-
-    <div><h1>Personajes</h1>
-    <button onClick={hadleChangePage}>Muestra mas</button>
-
-
+    <div>
+      <h1>Personajes</h1>
+      <button onClick={hadleChangePage}>Muestra mas</button>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
