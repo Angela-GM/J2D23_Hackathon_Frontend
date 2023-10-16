3f6134dae0856f5c353873baf8be3725b28b5510
Angela-GM <angelagm0112@gmail.com>
2023-10-16 12:45:15 +0200

    dar estilos al Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-   <>
+   <header className="flex items-center justify-center w-2/3 mx-auto">
    <img src="../../public/Rick_and_Morty.svg" alt="logo Rick y Morty" />
-   </>
+   </header>
   )
 }
 
