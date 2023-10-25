d3e2bd7f5aaf07bd4df37d8420fe5c582aba8fcc
Angela-GM <angelagm0112@gmail.com>
2023-10-25 14:31:23 +0200

    arreglar ruta img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
    <header className="flex items-center justify-center w-2/4 mx-auto py-8">
-   <img src="/public/Rick_and_Morty.svg" alt="logo Rick y Morty" />
+   <img src="/src/assets/Rick_and_Morty.svg" alt="logo Rick y Morty" />
    </header>
   )
 }
