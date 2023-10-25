8aba4f98b7335d3736c3e76a6a71266039d260ae
Angela-GM <angelagm0112@gmail.com>
2023-10-25 16:11:23 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,29 @@
+import { Github, Linkedin } from "lucide-react";
+
 function Footer() {
+  const now = new Date();
+  const year = now.getFullYear();
   return (
-    <div>Footer</div>
-  )
+    <footer className="text-center py-4 text-white">
+      <p className="flex justify-center gap-4">
+        <a
+          href="https://www.linkedin.com/in/angela-garcia-mu/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Linkedin />
+        </a>
+        <a
+          href="https://github.com/Angela-GM"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+      </p>
+      <small>&copy; {year} Angela Garcia. Todos los derechos reservados.</small>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
