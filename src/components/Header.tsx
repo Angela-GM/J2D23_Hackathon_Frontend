d6220ec9e21ba769137b6af9bad675c158fb6821
Angela-GM <angelagm0112@gmail.com>
2023-10-25 15:50:43 +0200

    actualizar readme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+
+
 function Header() {
   return (
    <header className="flex items-center justify-center w-2/4 mx-auto py-8">
-   <img src="/src/assets/Rick_and_Morty.svg" alt="logo Rick y Morty" />
-   <img src="../../public/Rick_and_Morty.svg" alt="logo Rick y Morty" />
+   <img src="/public/Rick_and_Morty.svg" alt="logo Rick y Morty" />
+
 
    </header>
   )
